fb050628fcf8723a1c9a78b03c03cc283bb7ceb2
Luke Pilling <luke.pilling@gmail.com>
2014-08-27 15:46:12 +0100

    Update scatterBox.R
    Changed some default parameters to make it prettier and more logical to adjust the points vs. the box/whiskers.

--- scatterBox.R
+++ scatterBox.R
@@ -4,35 +4,38 @@
 
 #### Inputs/options
 #### ==============
-#### x.var   ::  {REQUIRED} the numeric y-axis variable
-#### y.var   ::  {REQUIRED} the categorical x-axis variable +++must be numeric+++
-#### x.lab   ::  {optional} label for x-axis
-#### y.lab   ::  {optional} label for y-axis
-#### y.lim   ::  {optional} vector of length 2, with the minimum and maximum y-axis values
-#### main    ::  {optional} title of plot
-#### labels  ::  {optional} a vector of labels for the categories of y.var {optional}
-#### showN   ::  {default=TRUE} change to FALSE to not show # of samples in each category
+#### x.var      ::  {REQUIRED} the categorical x-axis variable +++must be numeric+++
+#### y.var      ::  {REQUIRED} the numeric y-axis variable
+#### x.lab      ::  {optional} label for x-axis
+#### y.lab      ::  {optional} label for y-axis
+#### y.lim      ::  {optional} vector of length 2, with the minimum and maximum y-axis values
+#### main       ::  {optional} title of plot
+#### labels     ::  {optional} a vector of labels for the categories of y.var {optional}
+#### showN      ::  {default=TRUE} change to FALSE to not show # of samples in each category
+#### doScatter  ::  {default=TRUE} whether or not to show the scatter points
+#### doBox      ::  {default=TRUE} whether or not to show the box plots  --  can just show scatter categories if set to FALSE
 
 #### Colours
 #### =======
-#### points.col    ::  {default="grey60"} colour of scatter points
+#### points.col    ::  {default="grey35"} colour of scatter points
+#### points.alpha  ::  {default="0.8"} transparency of scatter points
 #### bg.col        ::  {default="grey96"} colour of background of plot
 #### box.col       ::  {default="grey70"} fill colour of boxes
-#### box.alpha     ::  {default=0.75}     transparency of box fill (min=0, max=1)
 #### border.col    ::  {default="grey20"} border colour of boxes
-#### border.alpha  ::  {default=0.8}      transparency of box borders (min=0, max=1)
 
 
-scatterBox  <- function(x.var=NULL, y.var=NULL, y.lim=NA, 
-                        x.lab="",   y.lab="", main="", labels=NA, showN=TRUE,
-                        points.col="grey60", bg.col="grey96",
-                        box.col="grey70", box.alpha=0.75, border.col="grey20", border.alpha=0.8
-                       )  {
+scatterBox  <- function(x.var=stop("\n\n## Need to specify a valid vector of numeric categories for the X-axis using [x.var=...]\n\n"), 
+                        y.var=stop("\n\n## Need to specify a valid vector of numeric values for the Y-axis using [y.var=...]\n\n"), 
+                        y.lim=NA, 
+                        x.lab="",   y.lab="", main="", subtitle="", labels=NA, showN=TRUE,
+                        points.col="grey35", points.alpha=0.8,
+                        bg.col="grey96",
+                        box.col="grey70", border.col="grey20",
+                        doScatter=T, doBox=T
+                       )  
+{
     
     ## check X/Y variables appropriate -- return error if not
-    if (is.null(x.var))  stop("\n\n## Need to specify a valid vector of numeric categories for the X-axis using [x.var=...]\n\n")
-    if (is.null(y.var))  stop("\n\n## Need to specify a valid vector of numeric values for the Y-axis using [y.var=...]\n\n")
-
     if (!is.numeric(x.var))  stop("\n\n## Vector of values for X-axis needs to be numeric - this means they appear in the order you want!\n\n")
     if (!is.numeric(y.var))  stop("\n\n## Vector of values for Y-axis needs to be numeric\n\n")
     
@@ -68,21 +71,23 @@ scatterBox  <- function(x.var=NULL, y.var=NULL, y.lim=NA,
             xlab=x.lab
         )
     rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col=bg.col)
-
-    
-    ## add raw data (with jitter on Xaxis)
-    x.var.points <- jitter( x.var + xDiff )
-    points(x.var.points, y.var, pch=16, col=points.col)
     
     ## add boxes (semi-transparent)
+    if (doBox)
+    {
     boxplot(y.var ~ x.var, 
             xaxt = "n", 
             add = TRUE, 
-            col=adjustcolor(box.col, alpha.f=box.alpha),
-            border=adjustcolor(border.col, alpha.f=border.alpha),
+            col=box.col,
+            border=border.col,
             pch=16,
             outline=FALSE
            )
+    }
+
+    ## add raw data (with jitter on Xaxis)
+    x.var.points <- jitter( x.var + xDiff )
+    if (doScatter)  points(x.var.points, y.var, pch=16, col=adjustcolor(points.col, alpha.f=points.alpha))
     
     ## add category labels
     if (is.na(labels[1]))  labels <- as.vector(names(table(x.var)))
@@ -99,6 +104,9 @@ scatterBox  <- function(x.var=NULL, y.var=NULL, y.lim=NA,
         text( x=i, y=ylim[1]+span.3p, table(x.var)[[i]], cex=1.1)
       }
     }
+
+    # add subtitle
+    mtext(subtitle, line=0.3, cex=0.8)
 
     
 }  ## end of scatterBox function
